9b4e6fc95809783c7c8b974c040c3614669f16c2
monicacholico <monicachol@hotmail.com>
2018-05-15 15:52:13 -0500

    Added methods to create directory and file, added method to create an Array of Contact list

---
+++ src/FileIO.java
@@ -0,0 +1,70 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class FileIO {
+
+    public static void main(String[] args) {
+
+        String directory = "data";
+        String filename = "contacts.txt";
+
+//                createNewFile( directory, filename);
+
+//        ArrayList<String> contacts =
+
+    }
+
+
+    public static void createNewFile(String directory, String filename){
+        Path contactDirectory = Paths.get(directory);
+        Path contactFile = Paths.get(directory, filename);
+
+        try{
+            if (Files.notExists(contactDirectory)){
+                Files.createDirectories(contactDirectory);
+            }
+
+            if (Files.notExists(contactFile)){
+                Files.createFile(contactFile);
+            }
+        }catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+
+    public static void writeContactsToFile(ArrayList<String> list, String directory, String filename) throws IOException{
+
+        Path filepath = Paths.get(directory, filename);
+        Files.write(filepath, list, StandardOpenOption.APPEND);
+
+    }
+
+
+    public static ArrayList<String> makeContacts(){
+        ArrayList<String> list = new ArrayList<>();
+        Input input = new Input();
+        String contact;
+
+
+        contact = input.getString("Please input the contact you want to add to the list");
+        list.add(contact);
+
+
+        return list;
+
+    }
+
+
+
+
+
+
+}
