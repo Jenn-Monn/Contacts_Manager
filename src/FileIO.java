ad433dffb6276923a2a087491d346027344972a6
monicacholico <monicachol@hotmail.com>
2018-05-17 10:54:56 -0500

    added menu to display options for user

--- src/FileIO.java
+++ src/FileIO.java
@@ -17,6 +17,14 @@ public class FileIO {
 
                 createNewFile( directory, filename);
 
+        System.out.println("Welcome to your Phone Book!");
+        System.out.println("   1.   View all contacts ");
+        System.out.println("   2.   Add a new contact");
+        System.out.println("   3.   Search a contact by name");
+        System.out.println("   4.   Delete an existing contact");
+        System.out.println("   5.   Exit.");
+        System.out.println("Enter an option (1, 2, 3, 4 or 5) :");
+
         ArrayList<Object> contacts = makeContacts();
         System.out.println(contacts);
 
